1d573f016d44a95d70592f3b0192df77fded6608
MattGrizzly <magrishams@gmail.com>
2022-05-26 02:09:34 -0500

    inquire npm added and initial inquirer prompt added.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
     }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-      console.log(profileItem)
-    };
+  ])
+  .then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
